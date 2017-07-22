60dc30555643f15b98c9d190cf43f4d7fbdde817
JamesZed <491788533@qq.com>
2017-07-22 14:25:40 +0800

    测试6

---
+++ app/Entity/Pdt_Content.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Entity;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Pdt_Content extends Model
+{
+    protected $table = 'Pdt_Content';
+    protected $primaryKey = 'id';
+}
---
+++ app/Entity/Pdt_Images.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Entity;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Pdt_Images extends Model
+{
+    protected $table = 'Pdt_Images';
+    protected $primaryKey = 'id';
+}
